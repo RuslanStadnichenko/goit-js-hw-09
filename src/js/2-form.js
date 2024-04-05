07606a07876a1a93b90c0777934f83a9304dd34f
RuslanStadnichenko <imanpepsi77@gmail.com>
2024-04-05 19:12:49 +0300

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -15,7 +15,8 @@ const fillFeedbackFormFields = () => {
     }
    } catch (err) {
        console.error('Error while filling form fields:', err);
-   }
+  }
+
 };
 
 const saveFormState = (event) => {
@@ -30,10 +31,18 @@ const saveFormState = (event) => {
 
 const onFeedbackFormSubmit = event => {
   event.preventDefault();
+  const { email, message } = event.currentTarget.elements
+  if (email.value.trim() === "" || message.value.trim() === "") {
+    return alert("Не всі поля заповнені")
+  }
+
+  console.log('Email:', email.value.trim());
+  console.log('Message:', message.value.trim());
 
   localStorage.removeItem(storageKey);
   feedbackFormEl.reset();
 };
+
 
 fillFeedbackFormFields();
 
